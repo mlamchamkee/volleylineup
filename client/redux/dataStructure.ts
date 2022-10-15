3bf61871ee902faa98be869044c4dbc2236176c5
Michael Lam Cham Kee <mlamchamkee@gmail.com>
2022-10-15 08:37:06 -0400

    add player class

---
+++ client/redux/dataStructure.ts
@@ -0,0 +1,12 @@
+import { PositionType }  from '../../types';
+
+export class Player {
+  id: number;
+  name: string;
+  position: PositionType;
+  constructor (id: number, name: string, position: PositionType){
+    this.id = id;
+    this.name = name;
+    this.position = position;
+  }
+}
