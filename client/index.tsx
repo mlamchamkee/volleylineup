a0bd58c2449a12d1edfc193c726045e6dc3a6bf7
Michael Lam Cham Kee <mlamchamkee@gmail.com>
2022-10-15 14:44:19 -0400

    add logo and some style

--- client/index.tsx
+++ client/index.tsx
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { store } from './redux/store';
-import App from './App';
-
+import AppNavbar from './components/AppNavbar';
+import Court from './containers/Court';
+import Roster from './containers/Roster';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {MAIN_THEME} from '../constants'
 const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: MAIN_THEME.color,
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#606F7B',
+      contrastText: '#fff'
+    },
+    info: {
+      main: '#8795A1',
+      contrastText: '#fff'
+    }
+  },
+});
+
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <AppNavbar />
+      <Roster />
+      <Court />
+    </Provider>
+  </ThemeProvider>
 );
